9628f4f51331cccbfe6cdc4b7e00971d6b3c15fc
Thongpak Pongsilathong <thongpak21@gmail.com>
2019-05-07 23:05:50 +0700

    remove unused

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../View/Components/Home/'
 import PrivateRoute from './PrivatedRoute'
 import LoginForm from '../View/Components/Login'
-import { isUser } from '../Config/firebase'
 
 const Routing = () => (
   <Switch>
